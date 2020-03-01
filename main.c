1a2aed799df673d10fc1ca7c6391d11f2372a40e
Pendergaster <patrik.broms95@gmail.com>
2020-03-01 20:43:44 +0200

    Header apis done and url parsing

--- main.c
+++ main.c
@@ -25,9 +25,16 @@ char* SERVER_PORT = "12913";
 
 static void server_run();
 
+typedef struct HeaderField {
+    char* name, *value;
+} HeaderField ;
 
 typedef struct Header {
-    char* name, *value;
+    char*       method;
+    char*       protocol;
+    char*       uri;
+    HeaderField fields[17];
+    char*       payload;
 } Header;
 
 
@@ -55,50 +62,198 @@ main(int argc, char** argv) {
 
 // -1 not found
 static i32
-header_get_correct(Header* res, Header* headers, u32 numHeaders,const char* name) {
+header_get_correct_field(HeaderField* res, Header* header, const char* name) {
 
-    for(u32 i = 0; i < numHeaders; i++) {
-        printf("ale ale ale \n");
-        if(headers[i].name == NULL)
+    for(u32 i = 0; i < ARRAY_SIZE(header->fields); i++) {
+        if(header->fields[i].name == NULL)
             return -1;
-        if (strcmp(headers[i].name, name) == 0) {
-            *res = headers[i];
+        if (strcmp(header->fields[i].name, name) == 0) {
+            *res = header->fields[i];
             return 0;
         }
     }
     return -1;
 }
 
-static const char okMessage[] = "HTTP/1.1 200 OK\r\n\r\n";
+typedef enum HTTPStatus{
+    HTTP_OK,
+    HTTP_NOT_FOUND,
+} HTTPStatus;
+
+
+
+static const char okMessage[] =
+"HTTP/1.1 200 OK\r\n"
+"Content-Type: %s\r\n"
+"Content-Length: %d\n"
+"\r\n";
+
+
+static const char notFoundMessage[] =
+"HTTP/1.1 404 Not Found\r\n"
+"Content-Type: %s\r\n"
+"Content-Length: %d\n"
+"\r\n";
+
 
 static void
-client_post(i32 clientfd, i32 payloadSize) {
+header_parse(Header* header, char* buf) {
 
-    write(clientfd, okMessage, ARRAY_SIZE(okMessage));
+    // "GET" or "POST"
+    header->method = strtok(buf, " \t\r\n");
+    // "/index.html" things before '?'
+    header->uri    = strtok(NULL, " \t");
+    // "HTTP/1.1"
+    header->protocol   = strtok(NULL, " \t\r\n");
+
+    fprintf(stderr, "\x1b[32m + [%s] [%s] %s\x1b[0m\n", header->method, header->protocol, header->uri);
+
+    // Parse header
+    for(u32 i = 0; i < ARRAY_SIZE(header->fields); i++) {
+        header->fields[i].name = strtok(NULL, "\r\n: \t");
+
+        if (! header->fields[i].name)
+            break;
+
+        char* val =  strtok(NULL, "\r\n");
+
+        while(*val && *val==' ')
+            val++;
+
+        header->fields[i].value = val;
+
+        fprintf(stderr, "[H] %s: %s\n", header->fields[i].name, header->fields[i].value);
+
+        // end of value string
+        header->payload = header->fields[i].value + 1 + strlen( header->fields[i].value);
+        if (header->payload[1] == '\r' && header->payload[2] == '\n')
+            break;
+    }
+
+    header->payload = strtok(NULL, "\r\n");
+}
+
+static char*
+header_construct(HTTPStatus status,char* contentType, u32 contentLenght, u32* headerLen) {
+    // 999999 max contentLenght
+    if(contentLenght > 9999999) {
+        fprintf(stderr, "Too long content lenght, exiting..;\n");
+        fflush(stderr);
+        return NULL;
+    }
+    char* data = NULL;
+    if(status == HTTP_OK) {
+        data = (char*)malloc(sizeof(ARRAY_SIZE(okMessage) + 253));
+        *headerLen = sprintf(data, okMessage , contentType, contentLenght);
+    } else if(status == HTTP_NOT_FOUND){
+        data = (char*)malloc(sizeof(ARRAY_SIZE(notFoundMessage) + 253));
+        *headerLen = sprintf(data, notFoundMessage , contentType, contentLenght);
+    } else {
+        return NULL;
+    }
+
+    if(*headerLen > 0)
+        return data;
+    else {
+        fprintf(stderr, "Too long content lenght, exiting..;\n");
+        fflush(stderr);
+        return NULL;
+    }
+}
+
+
+
+static void
+client_post(i32 clientfd, Header* header) {
+
+
+
+    HeaderField contentLenght;
+    i32 payloadSize = 0;
+
+    if(header_get_correct_field(&contentLenght, header, "Content-Length") != 0) {
+        fprintf(stderr, "Did not foind content lengt for post\n");
+        return;
+    }
+
+    payloadSize = atol(contentLenght.value);
 
     char data[256];
-    int written = sprintf (data, "You POSTed %d bytes. \r\n", payloadSize);
-    if(written > 0) {
-        write(clientfd, data, written);
-    }
+    i32 contentLen = sprintf (data, "You POSTed %d bytes. \r\n", payloadSize);
+    if(contentLen >= 0)
+        return;
+
+    u32 headerLen = 0;
+    char* respHeader = header_construct(HTTP_OK, "text/html; charset=utf-8l", contentLen, &headerLen);
+    if(!respHeader)
+        return;
+
+    write(clientfd, respHeader, headerLen);
+    write(clientfd, data, contentLen);
+
+    free(respHeader);
+}
+
+static void
+client_get_index() {
 
 }
 
 static void
-client_get(i32 clientfd, Header* headers, u32 numHeaders) {
+client_get_dev(i32 clientfd, Header* header) {
 
-    printf("Getting! \n");
-    write(clientfd, okMessage, ARRAY_SIZE(okMessage) - 1);
-
-    Header userAgent = {};
-    if(header_get_correct(&userAgent, headers, numHeaders, "User-Agent") == -1)
+    HeaderField userAgent = {};
+    if(header_get_correct_field(&userAgent, header, "User-Agent") == -1)
         return;
 
     char data[256];
-    int written = sprintf (data, "You are using %s", userAgent.value);
-    printf("sending %s", data);
-    if(written > 0) {
-        write(clientfd, data, written);
+    int contentLen = sprintf(data, "You are using %s\n", userAgent.value);
+    if(0 >= contentLen)
+        return;
+
+    u32 headerLen = 0;
+    char* respHeaders = header_construct(HTTP_OK, "text/html; charset=utf-8l", contentLen, &headerLen);
+    if(!respHeaders)
+        return;
+
+    printf("Headers (len %d): \n%s", headerLen, respHeaders);
+
+    write(clientfd, respHeaders, headerLen);
+
+    printf("%s", data);
+    write(clientfd, data, contentLen);
+
+    free(respHeaders);
+}
+
+static void
+client_get(i32 clientfd, Header* header) {
+
+
+    if(strcmp(header->uri, "/") == 0) {
+        client_get_index();
+    } else if (strcmp(header->uri, "/dev") == 0){
+        client_get_dev(clientfd, header);
+    }
+    else { // unknown
+
+        static const char unknownPage[] =
+            "<b>Unknown page, please go to </b>"
+            "<a href=\"http://127.0.0.1:12913\">Index</a>\n";
+
+        u32 headerLen = 0;
+        char* respHeaders = header_construct(HTTP_NOT_FOUND, "text/html; charset=utf-8l",
+                ARRAY_SIZE(unknownPage) - 1, &headerLen);
+
+        if(!respHeaders)
+            return;
+
+        printf("Headers (len %d): \n%s", headerLen, respHeaders);
+
+        write(clientfd, respHeaders, headerLen);
+
+        printf("%s", unknownPage);
+        write(clientfd, unknownPage, ARRAY_SIZE(unknownPage) - 1);
     }
 
 }
@@ -128,59 +283,14 @@ client_run(i32 clientfd) {
         // handle request
         buf[numCharacters] = '\0';
 
-        // extraxt tokens from string
+        Header header = {0};
+        header_parse(&header, buf);
 
-        // "GET" or "POST"
-        char* method = strtok(buf, " \t\r\n");
-        // "/index.html" things before '?'
-        char* uri    = strtok(NULL, " \t");
-        // "HTTP/1.1"
-        char* prot   = strtok(NULL, " \t\r\n");
-        (void) prot;
 
-        fprintf(stderr, "\x1b[32m + [%s] %s\x1b[0m\n", method, uri);
-
-        Header reqhdr[17] = {0};
-        char *payload = NULL;
-        for(u32 i = 0; i < ARRAY_SIZE(reqhdr); i++) {
-            reqhdr[i].name = strtok(NULL, "\r\n: \t");
-
-            if (!reqhdr[i].name)
-                break;
-
-            char* val =  strtok(NULL, "\r\n");
-
-            while(*val && *val==' ')
-                val++;
-
-            reqhdr[i].value = val;
-
-            fprintf(stderr, "[H] %s: %s\n", reqhdr[i].name, reqhdr[i].value);
-
-            // end of value string
-            payload = reqhdr[i].value + 1 + strlen(reqhdr[i].value);
-            if (payload[1] == '\r' && payload[2] == '\n')
-                break;
-        }
-
-        printf("rererere\n"); fflush(stdout);
-
-        payload = strtok(NULL, "\r\n");
-        printf("rererere\n"); fflush(stdout);
-
-        Header contentLenght;
-        i32 payloadSize = 0;
-
-        if(header_get_correct(&contentLenght, reqhdr, ARRAY_SIZE(reqhdr), "Content-Length") == 0) {
-            payloadSize = atol(contentLenght.value);
-        }
-
-        printf("rererere\n");
-
-        if(strcmp("GET", method)==0) {
-            client_get(clientfd, reqhdr, ARRAY_SIZE(reqhdr));
-        } else if(strcmp("POST", method)==0) {
-            client_post(clientfd, payloadSize);
+        if(strcmp("GET", header.method) == 0) {
+            client_get(clientfd, &header);
+        } else if(strcmp("POST", header.method) == 0) {
+            client_post(clientfd, &header);
         } else {
             client_default_method(clientfd);
         }
